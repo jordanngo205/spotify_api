f9acf958cfd053cef119682c617aaa0b85378f12
jordanngo205 <113134291+jordanngo205@users.noreply.github.com>
2023-08-23 19:10:45 +0000

    done login

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+
+ /* https://accounts.spotify.com/authorize?
+  client_id=5fe01282e94241328a84e7c5cc169164&redirect_uri=http:%2F%2Fexampl
+  e.com%2Fcallback&scope=user-read-private%20user-read
+  email&response_type=token&state=123 */ 
+  const CLIENT_ID = "adb075ad46114452bf4c07555f416991";
+  const SPOTIFY_AUTHORIZATION_ENDPOINT = "https://accounts.spotify.com/authorize";
+  const REDIRECT_URL_AFTER_LOGIN = "https://cautious-zebra-q7qr9rpxjw52xw4j-3000.app.github.dev/";
+  const SPACE_DELIMITER = "%20";
+  const SCOPES = ["user-read-currently-playing", "user-read-playback-state"];
+  const SCOPES_URL_PARAM = SCOPES. join (SPACE_DELIMITER);
+  const handleLogin = () => {
+    // Redirect user to Spotify login page
+    window.location = `${SPOTIFY_AUTHORIZATION_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL_AFTER_LOGIN}&scope=${SCOPES_URL_PARAM}&response_type=token&show_dialog=true`;
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="hero">
+          <h1 className="hero-title">Spotify Statistic</h1>
+          <p className="hero-subtitle">Millions of songs. No credit card needed.</p>
+          <button className="login-button" onClick={handleLogin}>
+            Log in with Spotify
+          </button>
+        </div>
       </header>
     </div>
   );
